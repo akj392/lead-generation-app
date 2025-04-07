9411a72e8a9d4ad6b5e492a6b820fd3cdd744677
akj392 <akj392@gmail.com>
2025-04-08 01:24:43 +0530

    added test coverage with karma-coverage and through "ng test" command

--- src/app/lead/components/lead-form/lead-form.component.spec.ts
+++ src/app/lead/components/lead-form/lead-form.component.spec.ts
@@ -2,11 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
 import { LeadService } from '../../services';
 import { LeadFormComponent } from './lead-form.component';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 describe('LeadFormComponent', () => {
   const mockLeadService = {
-    addLead: () => of({})
+    addLead: () => of()
   };
   let component: LeadFormComponent;
   let fixture: ComponentFixture<LeadFormComponent>;
@@ -14,6 +14,7 @@ describe('LeadFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LeadFormComponent],
+      imports: [ReactiveFormsModule],
       providers: [
         FormBuilder,
         { provide: LeadService, useValue: mockLeadService }
@@ -28,5 +29,46 @@ describe('LeadFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#onCreate', async () => {
+    component.leadForm.patchValue({
+      name: 'Test Name',
+      email: 'test@example.com'
+    });
+    const spy = spyOn(mockLeadService, 'addLead');
+    await component.onCreate();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should return true if the control is touched and invalid', () => {
+    const controlName = 'name';
+    const control = component.leadForm.controls[controlName];
+    control.markAsTouched();
+    control.setValue('');
+    expect(component.isFormControlInvalid(controlName)).toBeTrue();
+  });
+
+  it('should return false if the control is untouched', () => {
+    const controlName = 'name';
+    const control = component.leadForm.controls[controlName];
+    expect(component.isFormControlInvalid(controlName)).toBeFalse();
+  });
+
+  it('should return false if the control is valid', () => {
+    const controlName = 'email';
+    const control = component.leadForm.controls[controlName];
+    control.markAsTouched();
+    control.setValue('test@example.com');
+
+    expect(component.isFormControlInvalid(controlName)).toBeFalse();
+  });
+
+  it('should return false if the control is touched but still valid', () => {
+    const controlName = 'name';
+    const control = component.leadForm.controls[controlName];
+    control.markAsTouched();
+    control.setValue('Test Name');
+    expect(component.isFormControlInvalid(controlName)).toBeFalse();
   });
 });
